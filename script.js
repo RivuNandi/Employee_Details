bd192bac76cb78ea2d27af9082abc44e8dc4cab5
RivuNandi <109845353+RivuNandi@users.noreply.github.com>
2022-07-23 13:06:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+var selectedRow = null
+
+function onFormSubmit() {
+    if (validate()) {
+        var formData = readFormData();
+        if (selectedRow == null)
+            insertNewRecord(formData);
+        else
+            updateRecord(formData);
+        resetForm();
+    }
+}
+
+function readFormData() {
+    var formData = {};
+    formData["fullName"] = document.getElementById("fullName").value;
+    formData["age"] = document.getElementById("age").value;
+    formData["dob"] = document.getElementById("dob").value;
+    formData["gender"] = document.getElementById("gender").value;
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.fullName;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.age;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.dob;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.gender;
+    cell4 = newRow.insertCell(4);
+    cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+                       <a onClick="onDelete(this)">Delete</a>`;
+}
+
+function resetForm() {
+    document.getElementById("fullName").value = "";
+    document.getElementById("age").value = "";
+    document.getElementById("dob").value = "";
+    document.getElementById("gender").value = "";
+    selectedRow = null;
+}
+
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("age").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("dob").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("gender").value = selectedRow.cells[3].innerHTML;
+}
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.fullName;
+    selectedRow.cells[1].innerHTML = formData.age;
+    selectedRow.cells[2].innerHTML = formData.dob;
+    selectedRow.cells[3].innerHTML = formData.gender;
+}
+
+function onDelete(td) {
+    if (confirm('Are you sure to delete this record ?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById("employeeList").deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
+function validate() {
+    isValid = true;
+    if (document.getElementById("fullName").value == "") {
+        isValid = false;
+        document.getElementById("fullNameValidationError").classList.remove("hide");
+    } else {
+        isValid = true;
+        if (!document.getElementById("fullNameValidationError").classList.contains("hide"))
+            document.getElementById("fullNameValidationError").classList.add("hide");
+    }
+	var age_value = document.getElementById("age").value
+	var dob_value = document.getElementById("dob").value
+	
+	var dob_year_value = dob_value.split("-")[0]
+	
+	var dob_year = 2022 - age_value;
+	// console.log(dob_year)
+	if (dob_year != dob_year_value)
+	{
+		isValid = false
+		document.getElementById("dobValidationError").classList.remove("hide");
+	}
+	else {
+        isValid = true;
+        if (!document.getElementById("dobValidationError").classList.contains("hide"))
+            document.getElementById("dobValidationError").classList.add("hide");
+    }
+    return isValid;
+}
